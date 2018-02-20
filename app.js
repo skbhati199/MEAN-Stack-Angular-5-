64bdf3fb5ae55468592c85a430c1127e15d60dd9
skbhati <skbhati199@gmail.com>
2018-02-20 15:37:56 +0530

    books remove routers

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use('/book', book);
 
 // catch 404 and forward to error handler
